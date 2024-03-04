3603222323a836977eda6c2cd224a84e4530101b
Lambert Nnadi <40530031+lambo10@users.noreply.github.com>
2024-03-04 15:46:32 +0100

    new commit

--- app.js
+++ app.js
@@ -209,9 +209,12 @@ app.post('/claim-mining-balance', verifyToken, checkAuth, async (req, res) => {
 // Get user mining balance
 app.get('/get-mining-balance', verifyToken, checkAuth, async (req, res) => {
   const userId = req.userId;
-  console.log("[----------------]");
-  console.log(userId);
+  
   let balance = await getUserMinnedTokenBalnce(userId);
+
+  console.log("[----------------]");
+  console.log(balance);
+
   let minneAmount = parseFloat(process.env.MINNE_AMOUNT);
   let floatRatio = balance / minneAmount;
   if(balance >= process.env.MINNE_AMOUNT){
